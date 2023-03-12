26c8e8373dee1911843ed267c5b717c9b47d9be2
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-12 17:52:17 +0800

    Avatar

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { addDecorator } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/theme'
+
+addDecorator(storyFn => (
+    <ThemeProvider theme={theme}>
+        {storyFn()}
+    </ThemeProvider>
+))
+
+export const parameters = {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+        },
+    },
+}
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+    /** @description 主色调 */
+    primaryColor: '#4F9DDE',
+    /** @description 在线状态 */
+    green: '#34D859',
+    /** @description 离线状态 */
+    gray: 'rgba(24, 28, 47, 0.2)',
+}
+
+export default theme
