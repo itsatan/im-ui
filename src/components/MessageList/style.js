b40e9b24e77994392d736163fbf26b39acf5cc80
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-18 12:01:49 +0800

    MessageList

---
+++ src/components/MessageList/style.js
@@ -0,0 +1,13 @@
+import StyledMessageCard from 'components/MessageCard/style'
+import styled from 'styled-components'
+
+const ChatList = styled.div`
+    ${StyledMessageCard}{
+        margin-bottom: 20px;
+    }
+`
+
+const StyledMessageList = styled.div``
+
+export default StyledMessageList
+export { ChatList }
