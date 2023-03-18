85c3f2646744f98d4bb266fe86ddf9986fa962e3
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-18 12:51:58 +0800

    ChatBubble

---
+++ src/components/ChatBubble/chatBubble.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBubble from ".";
+
+export default {
+    title: "UI组件/ChatBubble",
+    component: ChatBubble,
+    decorators: [storyFn => <div style={{ padding: 24 }}>{storyFn()}</div>]
+};
+
+export const FromOthers = () => <ChatBubble time='昨天 下午14:26'>这是一条其他人发送的聊天信息</ChatBubble>
+
+export const Mine = () => <ChatBubble type='mine' time='昨天 下午16:30'>这是一条我自己发送的聊天信息</ChatBubble>
---
+++ src/components/ChatBubble/style.js
@@ -0,0 +1,65 @@
+import Icon from 'components/Icon'
+import Paragraph from 'components/Paragraph'
+import Text from 'components/Text'
+import styled, { css } from 'styled-components'
+
+const Time = styled(Paragraph).attrs({ type: 'secondary', size: 'small' })`
+    margin:6px;
+    margin-left: 24px;
+    word-spacing: 1rem;
+`
+
+const BubbleTip = styled(Icon)`
+    position: absolute;
+    bottom: -16%;
+    left: 0;
+    z-index:5;
+`
+
+const Bubble = styled.div`
+    padding: 15px 22px;
+    box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.1);
+    border-radius:100px;
+    position: relative;
+    z-index: 10;
+`
+
+const MessageText = styled(Text)``
+
+const typeVariants = {
+    mine: css`
+        ${Bubble}{
+            background-color: ${({ theme }) => theme.primaryColor};
+        }
+
+        ${BubbleTip}{
+            transform:rotateY(180deg);
+            left: unset;
+            right:0;
+
+            path {
+                fill:${({ theme }) => theme.primaryColor}
+            }
+        }
+
+        ${Time}{
+            text-align: right;
+            margin-left: 0;
+            margin-right:24px;
+        }
+
+        ${MessageText}{
+            color:white;
+        }
+    `
+}
+
+const StyledChatBubble = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    ${({ type }) => type && typeVariants[type]}
+`
+
+export default StyledChatBubble
+export { Time, BubbleTip, Bubble, MessageText }
