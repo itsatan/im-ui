63423a8d6a1485d8312cc1d8e6a9c5a0b57191ec
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-19 12:37:05 +0800

    Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import StyledFooter, { IconContainer, StylePopoverContent } from './style'
+import Input from 'components/Input'
+import Icon from 'components/Icon'
+import Button from 'components/Button'
+import Emoji from 'components/Emoji'
+import Popover from 'components/Popover'
+import { useTheme } from 'styled-components'
+
+import { ReactComponent as ClipIcon } from 'assets/icons/clip.svg'
+import { ReactComponent as SmileIcon } from 'assets/icons/smile.svg'
+import { ReactComponent as MicrophoneIcon } from 'assets/icons/microphone.svg'
+import { ReactComponent as PlaneIcon } from 'assets/icons/plane.svg'
+import { ReactComponent as OptionsIcon } from "assets/icons/options.svg"
+
+function Footer ({ children, ...rest }) {
+    const [emojiIconActive, setEmojiIconActive] = useState(false)
+    const theme = useTheme()
+    return (
+        <StyledFooter {...rest}>
+            <Input
+                placeholder='请输入想和对方说的话...'
+                prefix={<Icon icon={ClipIcon} />}
+                suffix={
+                    <IconContainer>
+                        <Popover
+                            content={<PopoverContent />}
+                            offset={{ x: '-25%' }}
+                            onVisible={() => setEmojiIconActive(true)}
+                            onHide={() => setEmojiIconActive(false)}
+                        >
+                            <Icon icon={SmileIcon} color={emojiIconActive ? undefined : theme.gray3} />
+                        </Popover>
+                        <Icon icon={MicrophoneIcon} />
+                        <Button size='52px'>
+                            <Icon
+                                icon={PlaneIcon}
+                                color='white'
+                                style={{ transform: 'translateX(-2px)' }}
+                            />
+                        </Button>
+                    </IconContainer>
+                }
+            />
+        </StyledFooter>
+    )
+}
+
+function PopoverContent (props) {
+    return (
+        <StylePopoverContent>
+            <Emoji label="smile">😊</Emoji>
+            <Emoji label="grinning">😆</Emoji>
+            <Emoji label="thumbup">👍</Emoji>
+            <Emoji label="indexfingerup">☝️</Emoji>
+            <Emoji label="ok">👌</Emoji>
+            <Emoji label="handsputtogether">🙏</Emoji>
+            <Emoji label="smilewithsunglasses">😎</Emoji>
+            <Emoji label="flexedbicep">💪</Emoji>
+            <Icon icon={OptionsIcon} style={{ marginLeft: "24px" }} />
+        </StylePopoverContent>
+    )
+}
+
+Footer.propTypes = {
+    children: PropTypes.any
+}
+
+export default Footer
