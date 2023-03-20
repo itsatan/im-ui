1ca0f09af6d572efa892c8e39c0835bf5d2a741b
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-20 23:00:42 +0800

    Switch

---
+++ src/components/Switch/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import StyledSwitch, { Checkbox, Slider } from './style'
+
+function Switch ({...rest}) {
+    return (
+        <StyledSwitch {...rest}>
+            <Checkbox />
+            <Slider />
+        </StyledSwitch>
+    )
+}
+
+Switch.propTypes = {}
+
+export default Switch
