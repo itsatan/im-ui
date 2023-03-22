2e11212044079c500678c31a5cdf8f56771a4e59
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-22 13:31:24 +0800

    updete MessageList and new FilterList

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -1,56 +1,32 @@
 import React from 'react'
 import StyledMessageList, { ChatList } from './style'
-import Filter from 'components/Filter'
-import Select from 'components/Select'
-import Option from 'components/Option'
-import Button from 'components/Button'
-import Icon from 'components/Icon'
-import Input from 'components/Input'
 import MessageCard from 'components/MessageCard'
 
-import { ReactComponent as Plus } from 'assets/icons/plus.svg'
 import face1 from 'assets/images/face-male-1.jpg'
+import FilterList from 'components/FilterList'
 
 function MessageList ({...rest }) {
     return (
         <StyledMessageList {...rest}>
-            <Input.Search />
-            <ChatFilter />
-            <ChatList>
-                {[1, 2, 3, 4, 5, 6].map((_, index) => (
-                    <MessageCard
-                        key={index}
-                        active={index === 3}
-                        replied={index % 3 === 0}
-                        avatarSrc={face1}
-                        name='李铭浩'
-                        avatarStatus='online'
-                        statusText='在线'
-                        time='3 小时之前'
-                        message='即使爬到最高的山上，一次也只能脚踏实地'
-                        unreadCount={2}
-                    />
-                ))}
-            </ChatList>
+            <FilterList options={['最新消息优先', '在线好友优先']} actionLabel='创建会话'>
+                <ChatList>
+                    {[1, 2, 3, 4, 5, 6].map((_, index) => (
+                        <MessageCard
+                            key={index}
+                            active={index === 3}
+                            replied={index % 3 === 0}
+                            avatarSrc={face1}
+                            name='李铭浩'
+                            avatarStatus='online'
+                            statusText='在线'
+                            time='3 小时之前'
+                            message='即使爬到最高的山上，一次也只能脚踏实地'
+                            unreadCount={2}
+                        />
+                    ))}
+                </ChatList>
+            </FilterList>
         </StyledMessageList>
-    )
-}
-
-function ChatFilter () {
-    return (
-        <Filter style={{ padding: '20px 0' }}>
-            <Filter.Filters label='列表排序'>
-                <Select>
-                    <Option>最新消息优先</Option>
-                    <Option>在线好友优先</Option>
-                </Select>
-            </Filter.Filters>
-            <Filter.Action label='创建会话'>
-                <Button>
-                    <Icon icon={Plus} width={12} height={12} />
-                </Button>
-            </Filter.Action>
-        </Filter>
     )
 }
 
