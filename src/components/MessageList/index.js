2ff87a95eac626255af82cb295054dd83b3d1f23
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-24 12:31:39 +0800

    Staggered Animation 配置

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -4,25 +4,38 @@ import MessageCard from 'components/MessageCard'
 
 import face1 from 'assets/images/face-male-1.jpg'
 import FilterList from 'components/FilterList'
+import { useTrail, animated } from 'react-spring'
 
-function MessageList ({...rest }) {
+function MessageList ({ ...rest }) {
+    const trailAnimes = useTrail(6, {
+        transform: "translate3d(0px, 0px, 0px)",
+        from: { transform: "translate3d(-50px, 0px, 0px)" },
+        config: {
+            mass: 0.8,
+            tension: 280,
+            friction: 20,
+        },
+        delay: 100,
+    });
     return (
         <StyledMessageList {...rest}>
             <FilterList options={['最新消息优先', '在线好友优先']} actionLabel='创建会话'>
                 <ChatList>
                     {[1, 2, 3, 4, 5, 6].map((_, index) => (
-                        <MessageCard
-                            key={index}
-                            active={index === 3}
-                            replied={index % 3 === 0}
-                            avatarSrc={face1}
-                            name='李铭浩'
-                            avatarStatus='online'
-                            statusText='在线'
-                            time='3 小时之前'
-                            message='即使爬到最高的山上，一次也只能脚踏实地'
-                            unreadCount={2}
-                        />
+                        <animated.div key={index} style={trailAnimes[index]}>
+                            <MessageCard
+                                key={index}
+                                active={index === 3}
+                                replied={index % 3 === 0}
+                                avatarSrc={face1}
+                                name='李铭浩'
+                                avatarStatus='online'
+                                statusText='在线'
+                                time='3 小时之前'
+                                message='即使爬到最高的山上，一次也只能脚踏实地'
+                                unreadCount={2}
+                            />
+                        </animated.div>
                     ))}
                 </ChatList>
             </FilterList>
