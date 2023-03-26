3df46426a9b52d3a49b5301157920ca242edf925
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-03-26 21:17:08 +0800

    Settings animation

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -9,23 +9,38 @@ import Seperator from 'components/Seperator'
 import { ReactComponent as ArrowMenuRight } from "assets/icons/arrowMenuRight.svg";
 import { Link } from 'react-router-dom'
 import 'styled-components/macro'
+import { useSpring, animated } from 'react-spring'
 
-function Settings ({children, ...rest }) {
+function Settings ({ children, ...rest }) {
+    const animeProps = useSpring({
+        transform: 'translate3d(0px, 0px, 0px)',
+        opacity: 1,
+        from: {
+            opacity: 0,
+            transform: 'translate3d(100px, 0px, 0px)',
+        },
+        config: {
+            tension:140,
+        },
+        delay:300
+    })
     return (
         <StyledSettings {...rest}>
-            <SettingsGroup groupName='隐私设置'>
-                <SettingsItem label='添加好友时需要验证' />
-                <SettingsItem label='推荐通讯录好友' description='上传的通讯录只用来匹配好友列表，本应用不会记录和发送任何信息给其他机构' />
-            </SettingsGroup>
-            <SettingsGroup groupName='通知设置'>
-                <SettingsItem label='新消息通知' />
-                <SettingsItem label='语音和视频通话提醒' />
-                <SettingsItem label='显示通知详情' />
-                <SettingsItem label='声音' />
-                <Link to='/settings/blocked' css={`text-decoration:none;color:inherit;`}>
-                    <SettingsItem label='查看已静音的好友列表' type='menu' />
-                </Link>
-            </SettingsGroup>
+            <animated.div style={animeProps}>
+                <SettingsGroup groupName='隐私设置'>
+                    <SettingsItem label='添加好友时需要验证' />
+                    <SettingsItem label='推荐通讯录好友' description='上传的通讯录只用来匹配好友列表，本应用不会记录和发送任何信息给其他机构' />
+                </SettingsGroup>
+                <SettingsGroup groupName='通知设置'>
+                    <SettingsItem label='新消息通知' />
+                    <SettingsItem label='语音和视频通话提醒' />
+                    <SettingsItem label='显示通知详情' />
+                    <SettingsItem label='声音' />
+                    <Link to='/settings/blocked' css={`text-decoration:none;color:inherit;`}>
+                        <SettingsItem label='查看已静音的好友列表' type='menu' />
+                    </Link>
+                </SettingsGroup>
+            </animated.div>
         </StyledSettings>
     )
 }
